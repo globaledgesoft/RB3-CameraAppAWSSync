9a09b2ca51157a459565bf5ef04cc3cd70dea14a
jinka2015 <44898260+jinka2015@users.noreply.github.com>
2020-01-28 20:13:40 +0530

    modified the file with right indentaions

--- BoardApplication/upload.py
+++ BoardApplication/upload.py
@@ -1,32 +1,12 @@
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# This file is licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-#
-#
-
-
 import boto3
-
 
 def upload(source_file, bucket_name, object_key):
     s3 = boto3.resource('s3')
-
-    # Uploads the source file to the specified s3 bucket by using a
-    # managed uploader. The uploader automatically splits large
-    # files and uploads parts in parallel for faster uploads.
+    
     try:
         s3.Bucket(bucket_name).upload_file(source_file, object_key)
     except Exception as e:
         print(e)
-
 
 def main():
     import argparse
@@ -39,8 +19,5 @@ def main():
 
     upload(args.source_file, args.bucket_name, args.object_key)
 
-
 if __name__ == '__main__':
     main()
-
-
